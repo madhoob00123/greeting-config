f4ada10efcd7e842c452b55ffa7e9fda05cd38f7
madhoob00123 <44482759+madhoob00123@users.noreply.github.com>
2019-02-06 11:41:29 -0600

    Add files via upload

---
+++ src/main/java/com/example/greetingconfig/quote/Quote.java
@@ -0,0 +1,14 @@
+package com.example.greetingconfig.quote;
+
+public class Quote {
+    private String text;
+    private String author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
